597feba4b0f1b3c22d4d389252a7024dbebae609
Radoslav Demirov <rdemirov@gmail.com>
2017-11-19 19:38:50 +0200

    WIP : Add sample default state

--- reducers/index.js
+++ reducers/index.js
@@ -1,12 +1,39 @@
 import actionTypes from '../actions/actionTypes'
 
-const defaultState ={};
+const defaultState ={
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event'
+        }
+      ]
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.'
+        }
+      ]
+    }
+  };
 
 const reducer = (state=defaultState,action) => {
   switch(action.type) {
     case actionTypes.GET_DECKS: 
       if(action.decks) return action.decks;
-      else return {}
+      else return defaultState
   }
     return state;
 }
