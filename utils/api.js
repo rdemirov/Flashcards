c8f84cec4d5adf98a7bb5cdf66958674c0f2b449
Radoslav Demirov <rdemirov@gmail.com>
2017-11-21 22:20:26 +0200

    fix: Setting of keys in Async storage

--- utils/api.js
+++ utils/api.js
@@ -2,38 +2,41 @@ import { AsyncStorage } from 'react-native'
 
 const DECKS_STORAGE_KEY = 'flashcards:decksStorage'
 
-export function fetchDecks () {
-  return AsyncStorage.getItem(DECKS_STORAGE_KEY)
-    .then((decks) => {
-        return JSON.parse(decks);
-    })
-}
-
-export function fetchDeck ({deckId}) {
+export function fetchDecks() {
     return AsyncStorage.getItem(DECKS_STORAGE_KEY)
-      .then((deck) => {
-          return JSON.parse(deck)[deckId];
-      })
-  }
-  
-
-export function saveDeckTitle ({ title,id }) {
-  return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
-    [id]: {
-        title:title,
-        questions:[]
-    }
-  }))
+        .then((decks) => {
+            return JSON.parse(decks);
+        })
 }
 
-export function addCardToDeck ({ deckId, card }) {
-    const {question,answer} = card;
-    return AsyncStorage.mergeItem(DECKS_STORAGE_KEY,JSON.stringify({
-        [deckId]:{
-                    questions:[{
-                        question,
-                        answer
-                    }]
+export function fetchDeck({ deckId }) {
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then((deck) => {
+            return JSON.parse(deck)[deckId];
+        })
+}
+
+
+export function saveDeckTitle({ title, id }) {
+    return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+        [id]: {
+            title: title,
+            questions: []
         }
     }))
-  }
+}
+
+export function addCardToDeck({ deckId, card }) {
+    const { question, answer } = card;
+    return AsyncStorage.getItem(DECKS_STORAGE_KEY)
+        .then((result) => {
+            let decks = JSON.parse(result);
+            let questions = decks[deckId].questions;
+            questions.push(card);
+            return AsyncStorage.mergeItem(DECKS_STORAGE_KEY, JSON.stringify({
+                [deckId]: {
+                    questions
+                }
+            }))
+        })
+}
