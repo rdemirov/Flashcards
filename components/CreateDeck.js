10642e3bf5846a7125419269202d47160c599de9
Radoslav Demirov <rdemirov@gmail.com>
2017-11-21 21:22:45 +0200

    style: style create deck view

--- components/CreateDeck.js
+++ components/CreateDeck.js
@@ -1,39 +1,43 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import { StyleSheet, Text, View,TextInput,TouchableHighlight } from 'react-native';
-import {saveDeckTitleAsync} from '../actions'
+import { connect } from 'react-redux'
+import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+import { saveDeckTitleAsync } from '../actions'
 class CreateDeck extends Component {
 
     constructor(props) {
         super(props);
-        this.state={};
-        this.handleInputChange=this.handleInputChange.bind(this);
+        this.state = {};
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     handleInputChange(text) {
-        this.setState({deckTitle:text})
+        this.setState({ deckTitle: text })
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Text >What is the title of your new deck?</Text>
+                <Text style={{ fontSize: 30, margin: 30,alignItems:'center' }} >What is the title of your new deck?</Text>
                 <TextInput style={styles.inputField}
-                 placeholder={'Enter deck title'} 
-                 value={this.state.deckTitle}
-                 onChangeText={this.handleInputChange}/>
-                <TouchableHighlight
-                underlayColor="white"
-                 style={styles.btn}
-                 onPress={()=>{
-                     const id = Date.now();
-                     this.props.saveDeckTitleAsync({title:this.state.deckTitle,id})
-                     .then((result)=> {
-                        this.props.navigation.navigate('DeckView',{deckId:id})}
-                     )}}
-               >
-                   <Text style={styles.buttonText}>Submit</Text> 
-                </TouchableHighlight>
+                    placeholder={'Enter deck title'}
+                    value={this.state.deckTitle}
+                    onChangeText={this.handleInputChange} />
+                <View style={{ margin: 20 }}>
+                    <TouchableHighlight
+                        underlayColor="white"
+                        style={styles.btn}
+                        onPress={() => {
+                            const id = Date.now();
+                            this.props.saveDeckTitleAsync({ title: this.state.deckTitle, id })
+                                .then((result) => {
+                                    this.props.navigation.navigate('DeckView', { deckId: id })
+                                }
+                                )
+                        }}
+                    >
+                        <Text style={styles.buttonText}>Submit</Text>
+                    </TouchableHighlight>
+                </View>
             </View>
         )
     }
@@ -48,27 +52,31 @@ export default connect(mapStateToProps, {
 })(CreateDeck)
 
 const styles = StyleSheet.create({
-    container:{
-        justifyContent:'center',
-        alignItems:'center'
+    container: {
+        justifyContent: 'space-around',
+        alignItems: 'center'
     },
-    inputField:{
+    inputField: {
         borderWidth: 1,
         borderColor: 'black',
-        width:250
+        width: 300,
+        height: 50,
+        margin: 30,
+        paddingLeft:20,
+        paddingRight:20
     },
     btn: {
         borderWidth: 1,
         borderColor: 'black',
-        backgroundColor:'black',
-        height:60,
-        width:200,
-        alignItems:'center',
+        backgroundColor: 'black',
+        height: 60,
+        width: 200,
+        alignItems: 'center',
         justifyContent: 'center',
-        borderRadius:8
+        borderRadius: 8
     },
     buttonText: {
-        fontSize:20,
-        color:'white'
+        fontSize: 20,
+        color: 'white'
     }
-  });
+});
