27b199ff95e0d101c572be8e73fc8e362f731e4e
Radoslav Demirov <rdemirov@gmail.com>
2017-11-21 11:37:22 +0200

    feat : Enable Show Answer /question toggle during quiz

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View,TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 export default class Quiz extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentQuestionIndex: 0,
+            totalQuestions: props.navigation.state.params.questions.length,
+            correctCount: 0,
+            showAnswer: false,
+            showResults: false
+        }
+     this.toggleQuestionAnswer=this.toggleQuestionAnswer.bind(this);
+    }
+
+    toggleQuestionAnswer() {
+         this.setState((state)=>({
+             ...state,
+             showAnswer:!state.showAnswer
+         }))
+    }
+
     render() {
-        const {navigation} = this.props;
+        const { questions } = this.props.navigation.state.params;
+        const totalQuestions = this.state.totalQuestions;
+        const showAnswer = this.state.showAnswer;
+        let questionIndex = this.state.currentQuestionIndex + 1;
+        let {correctCount,incorrectCount,showResults} = this.state;
         return (
             <View >
-               <Text>Quiz</Text>
+                <Text>{`${questionIndex}/${totalQuestions}`}</Text>
+                {!showResults && <View>
+                    <View>
+                        <Text>{showAnswer ? questions[this.state.currentQuestionIndex].answer : questions[this.state.currentQuestionIndex].question}</Text>
+                        <TouchableHighlight onPress={this.toggleQuestionAnswer}>
+                            <Text> {showAnswer ? 'Answer' : 'Question'}</Text>
+                        </TouchableHighlight>
+                    </View>
+                    <View>
+                        <TouchableHighlight>
+                            <Text>Correct</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight>
+                            <Text>Incorrect</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>}
+                {showResults && <View>
+                    <Text>{`You have ${(correctCount*100)/totalQuestions}% correct answers`}</Text>
+                </View>}
             </View>
         )
     }
