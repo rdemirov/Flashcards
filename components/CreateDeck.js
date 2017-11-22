6e668a26f79bcdf0c34ad04ab676243d31e8686a
Radoslav Demirov <rdemirov@gmail.com>
2017-11-22 17:39:49 +0200

    style : Format code

--- components/CreateDeck.js
+++ components/CreateDeck.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { StyleSheet, Text, View, TextInput, TouchableHighlight } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight
+} from 'react-native';
 import { saveDeckTitleAsync } from '../actions'
 class CreateDeck extends Component {
 
